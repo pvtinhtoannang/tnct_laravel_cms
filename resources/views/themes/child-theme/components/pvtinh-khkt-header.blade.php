c6adcf689e7dc4c9df492a26710be0c84d0366bc
pvtinhtoannang <49936064+pvtinhtoannang@users.noreply.github.com>
2020-05-15 16:05:03 +0700

    fix header

---
+++ resources/views/themes/child-theme/components/pvtinh-khkt-header.blade.php
@@ -0,0 +1,55 @@
+<section class="pvtinh-khkt-header blade">
+    <div class="container">
+        <div class="header-content">
+            <div class="logo-header">
+                <a href="/">
+                    <img src="/component-assets/images/logo.png" alt="">
+                </a>
+            </div>
+            <div class="menu-header-content">
+                <nav class="menu-header">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <img src="" alt="">
+                                <span>Các khoá học</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="" alt="">
+                                <span>Thư viện</span>
+                            </a>
+                            <ul>
+                                <li><a href="">Phần mềm</a></li>
+                                <li><a href="">Tài liệu</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="" alt="">
+                                <span>Hướng dẫn</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="" alt="">
+                                <span>Liên hệ</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="" alt="">
+                                <span>090.133.4444</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="login-button">
+                    <button>Đăng nhập</button>
+                    <button>Đăng ký</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
