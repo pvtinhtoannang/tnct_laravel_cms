e4a6ae82d24110fee2ee440a6e9127db6e8641b1
Minh Nhựt <minhnhuttn@users.noreply.github.com>
2020-05-15 10:50:34 +0700

    0515

---
+++ resources/views/themes/child-theme/components/mn-khkt-my-courses.blade.php
@@ -0,0 +1,10 @@
+@if($users_data)
+    <section class="mn-khkt-my-courses">
+        <div class="sec-heading">
+            <h1 class=""></h1>
+        </div>
+        <div class="container">
+
+        </div>
+    </section>
+@endif
--- resources/views/themes/child-theme/page.blade.php
+++ resources/views/themes/child-theme/page.blade.php
@@ -1,3 +1,5 @@
 @if(is_page('tai-khoan'))
-    @include ('themes.child-theme.components.mn-khkt-acount')
+    @include ('themes.child-theme.components.mn-khkt-mn-khkt-my-acount')
+@elseif(is_page('khoa-hoc-cua-toi'))
+    @include ('themes.child-theme.components.mn-khkt-my-courses')
 @endif
