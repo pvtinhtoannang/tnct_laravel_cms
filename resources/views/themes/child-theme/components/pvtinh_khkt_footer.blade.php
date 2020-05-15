2b70ddfc090a3059e80f9ffe71c5b207ace46451
pvtinhtoannang <49936064+pvtinhtoannang@users.noreply.github.com>
2020-05-15 11:18:38 +0700

    Updade Footer

--- resources/views/themes/child-theme/components/pvtinh_khkt_footer.blade.php
+++ resources/views/themes/child-theme/components/pvtinh_khkt_footer.blade.php
@@ -1,11 +1,12 @@
 <section class="pvtinh_khkt_footer">
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-6 col-md-3">
+        <div class="footer-content">
+            <div class="footer-item">
                 <div class="footer-logo">
                     <div class="logo">
                         <a href="/">
-                            <img src="/component-assets/images/logo-footer.png" alt="/component-assets/images/logo-footer.png">
+                            <img src="/component-assets/images/logo-footer.png"
+                                 alt="/component-assets/images/logo-footer.png">
                         </a>
                     </div>
                     <div class="title-footer-logo">
@@ -15,13 +16,19 @@
 
                 </div>
                 <div class="address-company">
-                    <div class="address"><strong>Trụ sở: </strong>Số 11/11D, hẻm 518, đường Bùi Hữu Nghĩa, Quận Bình Thuỷ, TP. Cần Thơ</div>
-                    <div class="address"><strong>Chi nhánh 1: </strong>Số 11/11D, hẻm 518, đường Bùi Hữu Nghĩa, Quận Bình Thuỷ, TP. Cần Thơ</div>
-                    <div class="address"><strong>Chi nhánh 2: </strong>Số 11/11D, hẻm 518, đường Bùi Hữu Nghĩa, Quận Bình Thuỷ, TP. Cần Thơ</div>
+                    <div class="address"><strong>Trụ sở: </strong>Số 11/11D, hẻm 518, đường Bùi Hữu Nghĩa, Quận Bình
+                        Thuỷ, TP. Cần Thơ
+                    </div>
+                    <div class="address"><strong>Chi nhánh 1: </strong>Số 11/11D, hẻm 518, đường Bùi Hữu Nghĩa, Quận
+                        Bình Thuỷ, TP. Cần Thơ
+                    </div>
+                    <div class="address"><strong>Chi nhánh 2: </strong>Số 11/11D, hẻm 518, đường Bùi Hữu Nghĩa, Quận
+                        Bình Thuỷ, TP. Cần Thơ
+                    </div>
                 </div>
             </div>
-            <div class="col-xs-12 col-sm-6 col-md-3">
-                <h2 class="title-footer">DANH MỤC</h2>
+            <div class="footer-item">
+                <h2 class="title-footer text-uppercase">DANH MỤC</h2>
                 <nav class="menu-footer">
                     <ul>
                         <li><a href="#">Kế toán xây dựng - công trình</a></li>
@@ -37,8 +44,8 @@
                     </ul>
                 </nav>
             </div>
-            <div class="col-xs-12 col-sm-6 col-md-3">
-                <h2 class="title-footer">DANH MỤC</h2>
+            <div class="footer-item">
+                <h2 class="title-footer text-uppercase">ĐIỀU KHOẢN SỬ DỤNG</h2>
                 <nav class="menu-footer">
                     <ul>
                         <li><a href="#">Quy chế hoạt động</a></li>
@@ -48,17 +55,24 @@
                     </ul>
                 </nav>
             </div>
-            <div class="col-xs-12 col-sm-6 col-md-3">
+            <div class="footer-item">
                 <div class="box-fanpage">
                     <div id="fb-root"></div>
-{{--                    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v7.0"></script>--}}
-{{--                    <div class="fb-page" data-href="https://www.facebook.com/thuegiaminh" data-tabs="timeline" data-width="" data-height="190" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/thuegiaminh" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/thuegiaminh">Đại Lý Thuế Gia Minh</a></blockquote></div>--}}
+                    <script async defer crossorigin="anonymous"
+                            src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v7.0"></script>
+                    <div class="fb-page" data-href="https://www.facebook.com/thuegiaminh" data-tabs="timeline"
+                         data-width="" data-height="190" data-small-header="false" data-adapt-container-width="true"
+                         data-hide-cover="false" data-show-facepile="true">
+                        <blockquote cite="https://www.facebook.com/thuegiaminh" class="fb-xfbml-parse-ignore"><a
+                                    href="https://www.facebook.com/thuegiaminh">Đại Lý Thuế Gia Minh</a></blockquote>
+                    </div>
 
                     <p>Theo dõi chúng tôi</p>
 
                     <div class="list-socials-network">
                         <ul>
-                            <li><a href="#"><img src="/component-assets/images/facebook-letter-logo.png" alt=""></a></li>
+                            <li><a href="#"><img src="/component-assets/images/facebook-letter-logo.png" alt=""></a>
+                            </li>
                             <li><a href="#"><img src="/component-assets/images/youtube-icon.png" alt=""></a></li>
                             <li><a href="#"><img src="/component-assets/images/zalo.png" alt=""></a></li>
                             <li><a href="#"><img src="/component-assets/images/vimeo.png" alt=""></a></li>
@@ -69,3 +83,10 @@
         </div>
     </div>
 </section>
+<div class="pvtinh-khkt-copyright">
+    <div class="container">
+        <p class="text-center"><a
+                    href="https://toannangcantho.com/" target="_blank">2020 © KHOA HOC KE TOAN All Rights Reserved.
+                Designed by Toannangcantho.com</a></p>
+    </div>
+</div>
