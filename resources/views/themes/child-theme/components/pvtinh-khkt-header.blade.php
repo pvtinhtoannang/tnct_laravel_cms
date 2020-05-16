cf2b714202351745202e38fdf835961da308fd25
pvtinhtoannang <49936064+pvtinhtoannang@users.noreply.github.com>
2020-05-16 08:12:28 +0700

    Header

--- resources/views/themes/child-theme/components/pvtinh-khkt-header.blade.php
+++ resources/views/themes/child-theme/components/pvtinh-khkt-header.blade.php
@@ -11,13 +11,15 @@
                     <ul>
                         <li>
                             <a href="#">
-                                <img src="" alt="">
+                                <img src="/component-assets/images/img-1.png" alt="" class="img">
+                                <img src="/component-assets/images/img-1-hover.png" alt="" class="img-hover">
                                 <span>Các khoá học</span>
                             </a>
                         </li>
                         <li>
                             <a href="#">
-                                <img src="" alt="">
+                                <img src="/component-assets/images/img-2.png" alt="" class="img">
+                                <img src="/component-assets/images/img-2-hover.png" alt="" class="img-hover">
                                 <span>Thư viện</span>
                             </a>
                             <ul>
@@ -27,27 +29,31 @@
                         </li>
                         <li>
                             <a href="#">
-                                <img src="" alt="">
+                                <img src="/component-assets/images/img-3.png" alt="" class="img">
+                                <img src="/component-assets/images/img-3-hover.png" alt="" class="img-hover">
                                 <span>Hướng dẫn</span>
                             </a>
                         </li>
                         <li>
                             <a href="#">
-                                <img src="" alt="">
+                                <img src="/component-assets/images/img-4.png" alt="" class="img">
+                                <img src="/component-assets/images/img-4-hover.png" alt="" class="img-hover">
                                 <span>Liên hệ</span>
                             </a>
                         </li>
                         <li>
                             <a href="#">
-                                <img src="" alt="">
+                                <img src="/component-assets/images/img-5.png" alt="" class="img">
+                                <img src="/component-assets/images/img-5-hover.png" alt="" class="img-hover">
                                 <span>090.133.4444</span>
                             </a>
                         </li>
                     </ul>
                 </nav>
                 <div class="login-button">
-                    <button>Đăng nhập</button>
-                    <button>Đăng ký</button>
+                    <button class="">Đăng nhập</button>
+                    <button class="">Đăng ký</button>
+                    <button class="menu-mobile"><i class="fa fa-bars"></i> Menu</button>
                 </div>
             </div>
         </div>
