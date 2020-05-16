db57840c9d889a67c6a2f574f168a91f27cc2ea9
pvtinhtoannang <49936064+pvtinhtoannang@users.noreply.github.com>
2020-05-16 08:29:36 +0700

    Update page.blade.php

--- resources/views/themes/child-theme/page.blade.php
+++ resources/views/themes/child-theme/page.blade.php
@@ -3,7 +3,3 @@
 @elseif(is_page('khoa-hoc-cua-toi'))
     @include ('themes.child-theme.components.mn-khkt-my-courses')
 @endif
-@extends('themes.parent-theme.initialize')
-@section('content')
-    {{'page'}}
-@endsection 
