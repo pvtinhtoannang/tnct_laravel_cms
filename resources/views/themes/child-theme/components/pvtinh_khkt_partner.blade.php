1db4329d8f327f8ab2c42775cf419ba47eacb9c3
pvtinhtoannang <49936064+pvtinhtoannang@users.noreply.github.com>
2020-05-21 16:29:47 +0700

    Slider Partner

--- resources/views/themes/child-theme/components/pvtinh_khkt_partner.blade.php
+++ resources/views/themes/child-theme/components/pvtinh_khkt_partner.blade.php
@@ -1,13 +1,5 @@
 <section class="pvtinh_khkt_partner">
     <div class="container">
-        <div class="list-partner">
-            <?php for($i = 0; $i < 7; $i++): ?>
-            <div class="partner-item">
-                <a href="/" target="_blank">
-                    <img src="/component-assets/images/partner.jpg" alt="">
-                </a>
-            </div>
-            <?php endfor;?>
-        </div>
+        {{ render_slider(26, 'list-partner', '') }}
     </div>
 </section>
