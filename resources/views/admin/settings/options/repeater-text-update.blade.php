40d46cc222200158ded6f95db36236a077ae7e0c
pvtinhtoannang <49936064+pvtinhtoannang@users.noreply.github.com>
2020-05-28 15:48:43 +0700

    Fix option

--- resources/views/admin/settings/options/repeater-text-update.blade.php
+++ resources/views/admin/settings/options/repeater-text-update.blade.php
@@ -17,167 +17,98 @@
 
 <div class="repeater_update_parent">
     <div class="append-repeater_list_parent">
-        @foreach(json_decode($option['option_value']) as $index => $value)
-            @dump($value);
-            <div class="repeater-list-children @if($index == 0) repeater-list-children-0 @endif"
-                 data-parent-id="{{ $index }}">
-                <a class="btn btn-danger btn-delete-item-parent kt-font-light  @if($index == 0)  disabled  @endif"
-                   data-parent-id="1"><i
-                            class="fa fa-trash-alt"></i> Xoá nhóm
-                </a>
-                <div class="form-group">
-                    <label for="option_label">Tiêu đề cho repeater</label>
-                    <input type="text"
-                           name="option[{{ $option['option_name']}}][option_label_parent][]"
-                           class="form-control"
-                           aria-describedby="option_label"
-                           value="@if(isset($value->label)){{ $value->label }}@endif"
-                           placeholder="Nhập tiêu đề, ex: Tên website">
-                    <div class="kt-separator kt-separator--border-dashed kt-separator--space-lg"></div>
-                </div>
-                <div class="form-group">
-                    <label for="option_name">Slug cho repeater</label>
-                    <input type="text"
-                           name="option[{{ $option['option_name'] }}][option_slug_parent][]"
-                           class="form-control"
-                           aria-describedby="option_name"
-                           value="@if(isset($value->slug)){{ $value->slug }}@endif"
-                           placeholder="Slug viết không dấu và có dấu _ ở dưới, ex: tieu_de">
-                    <div class="kt-separator kt-separator--border-dashed kt-separator--space-lg"></div>
-                </div>
-                <div class="repeater-list-group">
-                    <div class="repeater-list-input">
-                        @php
 
-                            if(isset($value->children)){
-                                $slug = $value->children->slug;
-                                $label = $value->children->label;
-                            }else{
-                                $slug = '' ;
-                            }
-                        @endphp
-                        @if(is_array($slug))
-                            @for($i = 0; $i < sizeof($slug); $i++)
-                                <div class="form-group row align-items-center repeater-item-text repeater-item-text-{{$i}}"
-                                     data-id="{{ $i }}">
-                                    <div class="col-xs-12 col-md-12"><span>Vui lòng điền đầy đủ thông tin, không được bỏ trống và phải nhập đúng cấu trúc!</span>
-                                    </div>
-                                    <div class="col-md-7">
-                                        <div class="kt-form__group--inline">
-                                            <div class="kt-form__label">
-                                                <label>Nội dung:</label>
-                                            </div>
-                                            <div class="kt-form__control">
-                                                <input type="text"
-                                                       class="form-control"
-                                                       name="option[{{ $option['option_name'] }}][option_label_parent][][label]"
-                                                       placeholder="Nhập nội dung"
-                                                       value="{{ $label[$i]->label  }}"
-                                                >
-                                            </div>
-                                        </div>
-                                        <div class="d-md-none kt-margin-b-10"></div>
-                                    </div>
-                                    <div class="col-md-3">
-                                        <div class="kt-form__group--inline">
-                                            <div class="kt-form__label">
-                                                <label>Slug Sub - dùng cho
-                                                    dev</label>
-                                            </div>
-                                            <div class="kt-form__control">
-                                                <input type="text"
-                                                       class="form-control "
-                                                       name="option[{{ $option['option_name'] }}][option_slug_parent][][slug]"
-                                                       placeholder="Nhập slug"
-                                                       value="{{ $slug[$i]->slug  }}"
-                                                >
-                                            </div>
-                                        </div>
-                                        <div class="d-md-none kt-margin-b-10"></div>
-                                    </div>
-                                    <div class="col-md-2">
+        <div class="alert alert-primary" role="alert">
+            <div class="alert-text">{{ $option['option_label'] }}</div>
+            <input type="text" class="" name="option[{{$indexOption}}][{{ $option['option_name'] }}]"
+                   value="{{ $option['option_name'] }}">
 
-                                        <div class="kt-form__label">
-                                            <label>Xoá</label>
-                                        </div>
-                                        <a class="btn-sm btn btn-danger btn-pill btn-delete-item-input kt-font-light @if($i == 0) disabled @endif"
-                                           data-id="{{ $i }}">
-                                                                    <span>
-                                                                        <i class="la la-trash-o"></i>
-                                                                    </span>
-                                        </a>
-                                    </div>
-                                </div>
-                            @endfor
-                        @else
-                            @php
-                                $i = 1;
-                            @endphp
-                            <div class="form-group row align-items-center repeater-item-text repeater-item-text-0"
-                                 data-id="{{ $i }}">
-                                <div class="col-xs-12 col-md-12"><span>Vui lòng điền đầy đủ thông tin, không được bỏ trống và phải nhập đúng cấu trúc!</span>
-                                </div>
-                                <div class="col-md-7">
-                                    <div class="kt-form__group--inline">
-                                        <div class="kt-form__label">
-                                            <label>Nội dung:</label>
-                                        </div>
-                                        <div class="kt-form__control">
-                                            <input type="text"
-                                                   class="form-control reset-input"
-                                                   name="option[{{ $option['option_name'] }}][option_label_parent][][label]"
-                                                   placeholder="Nhập nội dung"
-                                                   value="{{ $value->label  }}"
-                                            >
-                                        </div>
-                                    </div>
-                                    <div class="d-md-none kt-margin-b-10"></div>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="kt-form__group--inline">
-                                        <div class="kt-form__label">
-                                            <label>Slug Sub - dùng cho
-                                                dev</label>
-                                        </div>
-                                        <div class="kt-form__control">
-                                            <input type="text"
-                                                   class="form-control reset-input"
-                                                   name="option[{{ $option['option_name'] }}][option_slug_parent][][slug]"
-                                                   placeholder="Nhập slug"
-                                                   value="{{ $value->slug  }}"
-                                            >
-                                        </div>
-                                    </div>
-                                    <div class="d-md-none kt-margin-b-10"></div>
-                                </div>
-                                <div class="col-md-2">
-                                    <div class="kt-form__label">
-                                        <label>Xoá</label>
-                                    </div>
-                                    <a class="btn-sm btn btn-danger btn-pill btn-delete-item-input disabled kt-font-light"
-                                       data-id="{{ $i }}">
-                                                                    <span>
-                                                                        <i class="la la-trash-o"></i>
-                                                                    </span>
-                                    </a>
-                                </div>
+        </div>
+        <div class="repeater-list-group repeater-list-group-repeater-text-update">
+            @php
+                $value_option = json_decode($option['option_value']);
+                $label = '';
+                $name = '';
+            @endphp
+
+
+            @foreach($value_option as $index => $column)
+                @php
+                    $label = $column->label;
+                    $name = $column->name;
+                @endphp
+                <div class="repeater-list-input repeater-list-input-0">
+                    <div class="alert-text">{{ $column->label }}</div>
+                    <input type="text" class="input-label-parent-repeater"
+                           data-option-name="{{ $option['option_name'] }}"
+                           name="option[{{$indexOption}}][{{ $option['option_name'] }}][{{$index}}][label]"
+                           value="{{ $label }}">
+                    <input type="text" class="input-name-parent-repeater"
+                           data-option-name="{{ $option['option_name'] }}"
+                           name="option[{{$indexOption}}][{{ $option['option_name'] }}][{{$index}}][name]"
+                           value="{{ $name }}">
+                    @php $i =0; @endphp
+
+                    @foreach($column->column as $key=>$item)
+                        <div class="form-group row align-items-center repeater-item-text repeater-item-text-update repeater-item-text-{{$i}}">
+                            <div class="col-xs-12 col-md-12">
+                                <span>Vui lòng điền đầy đủ thông tin, không được bỏ trống!</span>
                             </div>
-                        @endif
-                    </div>
-                    <div class="add-item">
-                        <a class="btn btn-info btn-add-item-update kt-font-light"
-                           data-last-id="{{ $i }}"><i
-                                    class="fa fa-plus-circle"></i> Thêm mới
-                        </a>
-                    </div>
+                            <div class="col-md-5">
+                                <div class="kt-form__group--inline">
+                                    <div class="kt-form__label">
+                                        <label>{{ $column->label }} - hidden input sau khi code xong</label>
+                                    </div>
+                                    <div class="kt-form__control">
+                                        <input type="text" class="form-control reset-input input-label-repeater-text"
+                                               data-name="{{  $option['option_name'] }}"
+                                               name="option[{{$indexOption}}][{{ $option['option_name'] }}][{{$index}}][column][{{$i}}][repeater_label]"
+                                               placeholder="" value="{{ $item->repeater_label }}">
+                                    </div>
+                                </div>
+                                <div class="d-md-none kt-margin-b-10"></div>
+                            </div>
+                            <div class="col-md-5">
+                                <div class="kt-form__group--inline">
+                                    <div class="kt-form__label">
+                                        <label>{{ $item->repeater_slug }} - hidden input sau khi code xong</label>
+                                    </div>
+                                    <div class="kt-form__control">
+                                        <input type="text" class="form-control reset-input input-slug-repeater-text"
+                                               data-name="{{  $option['option_name'] }}"
+                                               name="option[{{$indexOption}}][{{ $option['option_name'] }}][{{$index}}][column][{{$i}}][repeater_slug]"
+                                               placeholder="" value="{{ $item->repeater_slug }}">
+                                    </div>
+                                </div>
+                                <div class="d-md-none kt-margin-b-10"></div>
+                            </div>
+                            <div class="col-md-5">
+                                <div class="kt-form__group--inline">
+                                    <div class="kt-form__label">
+                                        <label>Giá trị</label>
+                                    </div>
+                                    <div class="kt-form__control">
+                                        <input type="text" class="form-control reset-input input-value-repeater-text"
+                                               data-name="{{  $option['option_name'] }}"
+                                               name="option[{{$indexOption}}][{{ $option['option_name'] }}][{{$index}}][column][{{$i}}][repeater_value]"
+                                               placeholder="" value="{{ $item->repeater_value }}">
+                                    </div>
+                                </div>
+                                <div class="d-md-none kt-margin-b-10"></div>
+                            </div>
+                        </div>
+
+                        @php $i++; @endphp
+                    @endforeach
                     <div class="kt-separator kt-separator--border-dashed kt-separator--space-lg"></div>
+
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
 
     <div class="add-item">
-        <a class="btn btn-info btn-add-item-parent-update kt-font-light"><i
+        <a class="btn btn-info btn-add-item-update kt-font-light" data-index-option="{{ $indexOption }}"><i
                     class="fa fa-plus-circle"></i> Thêm mới
         </a>
     </div>
--- resources/views/admin/settings/options/repeater-text.blade.php
+++ resources/views/admin/settings/options/repeater-text.blade.php
@@ -20,55 +20,56 @@
     <div class="append-repeater_list_parent">
 
         <div class="repeater-list-children" data-parent-id="1">
-            <a class="btn btn-danger btn-delete-item-parent kt-font-light" data-parent-id="1"><i
-                        class="fa fa-trash-alt"></i> Xoá nhóm
-            </a>
             <div class="form-group">
-                <label for="option_label">Tiêu đề cho repeater</label>
+                <label for="option_label">Tiêu đề <code>option_value_label</code></label>
                 <input type="text"
-                       name="option_label_parent[label][]" class="form-control reset-input"
+                       name="repeater[label]" class="form-control reset-input"
                        aria-describedby="option_label"
                        value=""
-                       placeholder="Nhập tiêu đề, ex: Tên website">
+                       placeholder="">
                 <div class="kt-separator kt-separator--border-dashed kt-separator--space-lg"></div>
             </div>
             <div class="form-group">
-                <label for="option_name">Slug cho repeater</label>
+                <label for="option_name">Slug <code>option_value_name</code></label>
                 <input type="text"
-                       name="option_slug_parent[slug][]" class="form-control reset-input"
+                       name="repeater[name]" class="form-control reset-input"
                        aria-describedby="option_name"
                        value=""
-                       placeholder="Slug viết không dấu và có dấu _ ở dưới, ex: tieu_de">
+                       placeholder="ex: tieu_de">
                 <div class="kt-separator kt-separator--border-dashed kt-separator--space-lg"></div>
             </div>
-            <div class="repeater-list-group">
+            <div class="repeater-list-group repeater-list-group-repeater-text">
                 <div class="repeater-list-input">
-                    <div class="form-group row align-items-center repeater-item-text"
+                    <div class="form-group row align-items-center repeater-item-text repeater-item-text-0"
                          data-id="1">
-                        <div class="col-xs-12 col-md-12"><span>Vui lòng điền đầy đủ thông tin, không được bỏ trống!</span>
+                        <div class="col-xs-12 col-md-12">
+                            <span>Vui lòng điền đầy đủ thông tin, không được bỏ trống!</span>
                         </div>
-                        <div class="col-md-7">
+                        <div class="col-md-5">
                             <div class="kt-form__group--inline">
                                 <div class="kt-form__label">
-                                    <label>Nội dung:</label>
+                                    <label>Label</label>
                                 </div>
                                 <div class="kt-form__control">
-                                    <input type="text" class="form-control reset-input"
-                                           name="option_label_parent[label][][label]"
-                                           placeholder="Nhập nội dung">
+                                    <input type="text" class="form-control reset-input input-label-repeater-text"
+                                           name="repeater[column][0][repeater_label]"
+                                           placeholder="">
+                                    <input type="hidden" class="form-control reset-input input-value-repeater-text"
+                                           name="repeater[column][1][repeater_value]"
+                                           placeholder="">
                                 </div>
                             </div>
                             <div class="d-md-none kt-margin-b-10"></div>
                         </div>
-                        <div class="col-md-3">
+                        <div class="col-md-5">
                             <div class="kt-form__group--inline">
                                 <div class="kt-form__label">
-                                    <label>Slug Sub - dùng cho dev</label>
+                                    <label>Name</label>
                                 </div>
                                 <div class="kt-form__control">
-                                    <input type="text" class="form-control reset-input"
-                                           name="option_slug_parent[slug][][slug]"
-                                           placeholder="Nhập slug">
+                                    <input type="text" class="form-control reset-input input-slug-repeater-text"
+                                           name="repeater[column][2][repeater_slug]"
+                                           placeholder="">
                                 </div>
                             </div>
                             <div class="d-md-none kt-margin-b-10"></div>
@@ -87,19 +88,13 @@
                     </div>
                 </div>
                 <div class="add-item">
-                    <a class="btn btn-info btn-add-item kt-font-light" data-last-id="1"><i
-                                class="fa fa-plus-circle"></i> Thêm mới
+                    <a class="btn btn-info btn-add-item-repeater-text kt-font-light">
+                        <i class="fa fa-plus-circle"></i> <span>Thêm mới </span>
                     </a>
-
-
                 </div>
                 <div class="kt-separator kt-separator--border-dashed kt-separator--space-lg"></div>
             </div>
         </div>
     </div>
-    <div class="add-item">
-        <a class="btn btn-info btn-add-item-parent kt-font-light" data-parent-last-id="1"><i
-                    class="fa fa-plus-circle"></i> Thêm nhóm mới
-        </a>
-    </div>
+
 </div>
